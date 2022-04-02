33abe2f31900abaf628255dc1a7ec9bff2e21598
hiep.buichinh <hiep.buichinh@gmail.com>
2022-04-02 16:04:46 +0700

    Fix spell

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -31,7 +31,7 @@ export default function resolver() {
             posts(root, args, context) {
                 return Post.findAll({
                     order: [
-                        ['ceratedAt', 'DESC']
+                        ['createdAt', 'DESC']
                     ]
                 });
             },
